ac9d374351f987ad1b8cfc34104d9d0c88e7f7df
ibalram <balramsinghrathore3@gmail.com>
2020-10-02 23:42:38 +0530

    Added: Master branch

---
+++ src/com/cg/addressbook/Executor.java
@@ -0,0 +1,9 @@
+package com.cg.addressbook;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		System.out.println("--------------------------------");
+	}
+}
